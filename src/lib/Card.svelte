a53380abf2d933cc4ad0d1d6afd39353e290f205
Sterling Smith <sterling12311@gmail.com>
2023-11-06 18:14:07 -0700

    cd into the correct directory

---
+++ src/lib/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title;
+  export let description;
+  export let button;
+  export let img;
+</script>
+
+<article class="m-5 bg-yellow-50">
+  <div class="my-5 bg-pink-50"></div>
+  <h2>{title}</h2>
+  <img src={img} alt="">
+  <p>{description}</p>
+  <button>{button}</button>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,34 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Card from "$lib/Card.svelte";
+
+  const cardObj = [{
+      title: 'Image title',
+      description: 'This is a painting',
+      button: 'This is button text',
+      img: 'https://picsum.photos/seed/picsum/200/300'
+  },
+  {
+      title: 'Image title',
+      description: 'This is a painting',
+      button: 'This is button text',
+      img: 'https://picsum.photos/seed/picsum/200/300'
+  },
+ {
+      title: 'Image title',
+      description: 'This is a painting',
+      button: 'This is button text',
+      img: 'https://picsum.photos/seed/picsum/200/300'
+  }]
+</script>
+
+<header>
+  <h1>Hello World! - SvelteKit Edition</h1>
+</header>
+<p>This is my honest attempt at SvelteKit</p>
+
+{#each cardObj as card}
+  <Card {...card}/>
+{/each}
+
+
+
